df611cdb8cd3ddda28acc8fbca6a91613279afc5
CyrusKell <cyfeye@gmail.com>
2021-08-31 13:49:33 -0400

    finished BuyStamps

--- src/BuyStamps.java
+++ src/BuyStamps.java
@@ -8,6 +8,18 @@
  *
  * @author cyrus.kell
  */
+import java.util.Scanner;
+
 public class BuyStamps {
-    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter amount of money you have to spend:");
+        double money = in.nextDouble();
+        
+        double pricePerStamp = 0.47;
+        
+        int numOfStamps  = (int)(money / pricePerStamp);
+        
+        System.out.println("You can buy " + numOfStamps + " first class stamps");
+    }
 }
