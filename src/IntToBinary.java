cfd0a06341b9f6b9baab7fb188e62a8f7bbe946d
Cyfeye kell <cyfeye@gmail.com>
2021-08-30 20:53:33 -0400

    changed IntToBinary output back to string and added handling for integer = 0

--- src/IntToBinary.java
+++ src/IntToBinary.java
@@ -13,17 +13,27 @@ import java.util.Scanner;
 public class IntToBinary {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Enter integer to be converted to binary:");
+        System.out.println("Enter an integer greater than 0 to be converted to binary:");
         int integer = in.nextInt();
+        
+        if (integer < 0) {
+            System.out.println("Error: integer must be greater than 0");
+            System.exit(0);
+        }
         
         System.out.println("Result: " + convertIntToBinaryString(integer));
     }
     
     public static String convertIntToBinaryString(int integer) {
         String binary = "";
-        while (integer > 0) {
+        
+        if (integer > 0) {
+            while (integer > 0) {
             binary = integer % 2 + binary;
             integer /= 2;
+            }
+        } else {
+            binary = "0";
         }
         
         return binary;
