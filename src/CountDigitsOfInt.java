1ceae36edb93447ef035ef0f97004f74346533c1
CyrusKell <cyfeye@gmail.com>
2021-09-01 14:02:58 -0400

    finished CountDigitsOfInt

---
+++ src/CountDigitsOfInt.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author cyrus.kell
+ */
+import java.util.Scanner;
+
+public class CountDigitsOfInt {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter an integer:");
+        int integer = in.nextInt();
+        
+        System.out.println("Your integer has " + countDigitsWithWhile(integer) + " digits");
+        System.out.println("Your integer has " + countDigitsWithoutWhile(integer) + " digits");
+    }
+    
+    public static int countDigitsWithWhile(int integer) {
+        int numOfDigits = 0;
+        
+        if (integer == 0) {
+            numOfDigits = 1;
+        } else {
+            while (integer > 0) {
+            numOfDigits++;
+            integer /= 10;
+            }
+        }
+        
+        
+        return numOfDigits;
+    }
+    
+    public static int countDigitsWithoutWhile(int integer) {
+        String integerAsStr = String.valueOf(integer);
+        
+        return integerAsStr.length();
+    }
+    
+//    public static int countDigitsWithoutWhile {}
+}
