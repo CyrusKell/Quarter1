fcffa67afb9967ab76aa7d4bfc9f2ac2f26037e9
CyrusKell <cyfeye@gmail.com>
2021-09-02 14:06:25 -0400

    converted CalculateCoins to use dollars * 100

--- src/CalculateCoins.java
+++ src/CalculateCoins.java
@@ -11,19 +11,19 @@
 import java.util.Scanner;
 
 public class CalculateCoins {
-    public static double dollars;
+    public static int dollars;
     
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter dollar amount to be converted to coins:");
-        dollars = in.nextDouble();
+        dollars = (int)(in.nextDouble() * 100);
         
         System.out.println(
             "You need " 
-            + countCoinsNeeded(.25) + " quarters, " 
-            + countCoinsNeeded(.10) + " dimes, " 
-            + countCoinsNeeded(.05) + " nickels, and " 
-            + countCoinsNeeded(.01) + " pennies"
+            + countCoinsNeeded(25) + " quarters, " 
+            + countCoinsNeeded(10) + " dimes, " 
+            + countCoinsNeeded(05) + " nickels, and " 
+            + countCoinsNeeded(01) + " pennies"
         );
     }
     
@@ -31,7 +31,6 @@ public class CalculateCoins {
         int numOfCoins = (int)(dollars / coinValue);
         
         dollars %= coinValue; 
-        dollars = (double)Math.round(dollars * 100) / 100;
         
         return numOfCoins;
     }
