1690353274760c6bfcd6b4655d339830ba180258
CyrusKell <cyfeye@gmail.com>
2021-09-02 14:02:14 -0400

    added one cent stamps to BuyStamps

--- src/BuyStamps.java
+++ src/BuyStamps.java
@@ -16,10 +16,13 @@ public class BuyStamps {
         System.out.println("Enter amount of money you have to spend:");
         double money = in.nextDouble();
         
-        double pricePerStamp = 0.47;
+        double pricePerFirstClassStamp = 0.47;
+        double pricePerOneCentStamp = 0.01;
         
-        int numOfStamps  = (int)(money / pricePerStamp);
+        int numOfFirstClassStamps  = (int)(money / pricePerFirstClassStamp);
+        int numOfOneCentStamps  = (int)(money / pricePerOneCentStamp);
         
-        System.out.println("You can buy " + numOfStamps + " first class stamps");
+        System.out.println("You can buy " + numOfFirstClassStamps + " first class stamps");
+        System.out.println("You can buy " + numOfOneCentStamps + " one cent stamps");
     }
 }
