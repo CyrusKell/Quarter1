734fd5849267e3d3e74963ab8f9c07aa377dc081
CyrusKell <cyfeye@gmail.com>
2021-09-02 13:50:33 -0400

    changed BinaryToDecimal to use long instead of String

--- src/BinaryToDecimal.java
+++ src/BinaryToDecimal.java
@@ -14,18 +14,29 @@ public class BinaryToDecimal {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter a binary number to be converted to decimal:");
-        String binary = in.nextLine();
+        long binary = in.nextLong();
+        validateBinary(binary);
         
         System.out.println("Result:" + convertBinaryToDec(binary));
     }
     
-    public static long convertBinaryToDec(String binary) {
+    public static void validateBinary(long binary) {
+        while (binary > 0) {
+            if (binary % 10 != 0 && binary % 10 != 1) {
+                System.out.println("Entered value is not valid binary");
+                System.exit(0);
+            }
+            binary /= 10;
+        }
+    }
+    
+    public static long convertBinaryToDec(long binary) {
         long decimal = 0;
         int power = 0;
         
-        for (int i = binary.length() - 1; i >= 0; i--) {
-            decimal += (Integer.parseInt(binary.charAt(i) + "") * Math.pow(2, power));
-            power++;
+        while (binary > 0) {
+            decimal += binary % 10 * Math.pow(2, power++);
+            binary /= 10;
         }
 
         return decimal;
