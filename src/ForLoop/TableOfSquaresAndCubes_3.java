c32f3d2f8dda42b223b4e1db15681f8686349611
Kell <cyrus.kell@school.aquinas-sta.org>
2021-10-05 14:07:30 -0400

    finished TableOfSquaresAndCubes_3

---
+++ src/ForLoop/TableOfSquaresAndCubes_3.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ForLoop;
+
+/**
+ *
+ * @author cyrus.kell
+ */
+import java.util.Scanner;
+
+public class TableOfSquaresAndCubes_3 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a positive integer less than 20:");
+        int n = in.nextInt();
+        
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i + " " + i * i + " " + i * i * i);
+        }
+    }
+}
