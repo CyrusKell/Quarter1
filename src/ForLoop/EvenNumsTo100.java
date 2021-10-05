0e0cd674803af705e4835e3f80494150ac189589
Kell <cyrus.kell@school.aquinas-sta.org>
2021-10-05 13:48:44 -0400

    finished EvenNumsTo100

---
+++ src/ForLoop/EvenNumsTo100.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ForLoop;
+
+/**
+ *
+ * @author cyrus.kell
+ */
+public class EvenNumsTo100 {
+    public static void main(String[] args) {
+        for (int i = 2; i <= 100; i += 2) {
+            System.out.println(i);
+        }
+    }
+}
