a2fc2ce57e568b6ec48a2916767ea8eb767bef72
CyrusKell <69449162+CyrusKell@users.noreply.github.com>
2021-10-07 14:56:58 -0400

    finished NumPyramid maybe

--- src/ForLoop/NumPyramid.java
+++ src/ForLoop/NumPyramid.java
@@ -11,24 +11,25 @@ package ForLoop;
  */
 import java.util.Scanner;
 
-public class NumTriangle {
+public class NumPyramid {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter number of lines:");
         int lines = in.nextInt();
         
         for (int base = 1; base <= lines * 2; base += 2) {
-            int additionalSpaces = (lines * 2 - base);
-            for (int space = 1; space <= (lines * 2 - base + additionalSpaces) / 2; space++) {
+            for (int space = 1; space <= (lines * 2 - base) / 2; space++) {
                 System.out.print(" ");
             }
             for (int i = base / 2 + 1; i < base; i++) {
-                System.out.print(i);
+                if (i >= 10) System.out.print(“ ”);
+                System.out.print(“ ” + i);
             }
             for (int i = base; i > base / 2; i--) {
                 System.out.print(i);
             }
             System.out.println();
         }
+
     }
 }
