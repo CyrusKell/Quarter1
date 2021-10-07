1f31fd20eff1d8635af698a0a3989a998a45d0b6
Kell <cyrus.kell@school.aquinas-sta.org>
2021-10-07 14:05:36 -0400

    finished FibonacciSequence

---
+++ src/ForLoop/FibonacciSequence.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ForLoop;
+
+/**
+ *
+ * @author cyrus.kell
+ */
+import java.util.Scanner;
+
+public class FibonacciSequence {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a positive integer");
+        int n = in.nextInt();
+        
+        int twoBefore = 1;
+        int oneBefore = 0;
+        for (int i = 1; i <= n; i++) {
+            int current = twoBefore + oneBefore;
+            System.out.print(current + ", ");
+            twoBefore = oneBefore;
+            oneBefore = current;
+        }
+    }
+}
