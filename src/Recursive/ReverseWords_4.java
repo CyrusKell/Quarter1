c849ed04b91c574c6b31e2bbbc26c50fd5cf0425
CyrusKell <cyfeye@gmail.com>
2021-10-14 17:03:27 -0400

    finished ReverseWords_4.java

---
+++ src/Recursive/ReverseWords_4.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Recursive;
+
+/**
+ *
+ * @author cyrus
+ */
+public class ReverseWords_4 {
+    public static void reverse(String str) {
+       int k = str.indexOf(" ");
+       if (k != -1) {
+           reverse(str.substring(k + 1));
+           System.out.println(str.substring(0, k));
+
+       }
+       if (k == -1) System.out.println(str);
+    }
+    
+    public static void main(String[] args) {
+        reverse("I am a student");
+    }
+}
