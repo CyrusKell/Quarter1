22a0d4e663dcbda984f226ae402a5ce1b3b4b1f4
Kell <cyrus.kell@school.aquinas-sta.org>
2021-10-21 14:13:23 -0400

    fixed 2

--- src/OneDArrays/LoopThruArray_2.java
+++ src/OneDArrays/LoopThruArray_2.java
@@ -9,9 +9,16 @@ package OneDArrays;
  *
  * @author cyrus.kell
  */
+import java.util.Scanner;
+
 public class LoopThruArray_2 {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
         int[] a = new int[8];
+        for (int i = 0; i < a.length; i++) {
+            System.out.println("Enter an integer:");
+            a[i] = in.nextInt();
+        }
         for (int i = 0; i < a.length; i++) {
             System.out.println(a[i]);
         }
