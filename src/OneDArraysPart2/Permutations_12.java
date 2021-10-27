7fdeffb8dbfca5ea57ae23c2e881a78d488e8e03
Kell <cyrus.kell@school.aquinas-sta.org>
2021-10-27 14:34:43 -0400

    started 12

---
+++ src/OneDArraysPart2/Permutations_12.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package OneDArraysPart2;
+
+/**
+ *
+ * @author cyrus.kell
+ */
+public class Permutations_12 {
+    public static void permutations(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+        int[] arr = {3,7,8};
+        permutations(arr);
+    }
+}
